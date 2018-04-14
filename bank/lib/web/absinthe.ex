476b6db9cec618d1ec41331c2f8ce4d4562d9278
Paulo Valente <polvalente@gmail.com>
2018-04-14 16:26:22 -0300

    finished adding GraphQL mutations

--- bank/lib/web/absinthe.ex
+++ bank/lib/web/absinthe.ex
@@ -5,10 +5,33 @@ defmodule Bank.Web.GraphQL.Schema do
   alias Bank.Web.GraphQL.Resolvers
 
   query do
-    @desc "Get accounts with given id"
+
+    @desc "Get accounts with given ID"
     field :account, :account do
       arg :id, non_null(:id)
       resolve &Resolvers.Account.get_account/3
     end
+
+  end
+
+  mutation do
+
+    @desc "Add funds to account with given ID"
+    field :add_funds, type: :account do
+      arg :account_id, non_null(:id)
+      arg :amount, non_null(:integer)
+
+      resolve &Resolvers.Account.add_funds/3
+    end
+
+    @desc "Transfer 'amount' from 'fromId' to 'toId'"
+    field :transfer, type: :account do
+      arg :from_id, non_null(:id)
+      arg :to_id, non_null(:id)
+      arg :amount, non_null(:integer)
+
+      resolve &Resolvers.Account.transfer/3
+    end
+
   end
 end
--- bank/lib/web/resolvers/account.ex
+++ bank/lib/web/resolvers/account.ex
@@ -1,8 +1,23 @@
 defmodule Bank.Web.GraphQL.Resolvers.Account do
+
   def get_account(_parent, %{id: id}, _resolution) do
     case Bank.Repo.get(Bank.Schemas.Account, id) do
       nil -> {:error, "Account with ID '#{id}' not found"} 
       account -> {:ok, %{id: account.account_id, balance: account.balance}}
+    end
+  end
+
+  def add_funds(_parent, %{amount: amount, account_id: id}, _resolution) do
+    case Bank.add_funds(id, amount) do
+      {:error, error} -> {:error, error}
+      _ -> get_account(nil, %{id: id}, nil) 
+    end
+  end
+
+  def transfer(_parent, %{from_id: sender, to_id: receiver, amount: amount}, _resolution) do
+    case Bank.transfer(sender, receiver, amount) do
+      {:error, error} -> {:error, error}
+      _ -> get_account(nil, %{id: sender}, nil) 
     end
   end
 end
